14e6e980d4780b2d2f466d7e7117ce724da29688
Dreaky <dvd3033@gmail.com>
2016-12-19 23:28:27 +0100

    Initial commit

---
+++ src/main/java/com/DreVDs/core/HelloWord.java
@@ -0,0 +1,11 @@
+package com.DreVDs.core;
+
+/**
+ * Hello world class
+ * Created by DreVDs on 12/19/16.
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
